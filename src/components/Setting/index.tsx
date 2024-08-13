746523ceb5ccff37fb1bfc08a96bc1a4c2167be0
mudssky <mudssky@gmail.com>
2024-08-13 11:37:21 +0800

    ✨ feat: settings layout and ComponentAttr

--- src/components/Setting/index.tsx
+++ src/components/Setting/index.tsx
@@ -1,10 +1,28 @@
 import { useComponetsStore } from '@/stores/components'
+import { Segmented } from 'antd'
+import { useState } from 'react'
+import { ComponentAttr } from './components/ComponentAttr'
+import { ComponentEvent } from './components/ComponentEvent'
+import { ComponentStyle } from './components/ComponentStyle'
 
 export function Setting() {
-  const { components } = useComponetsStore()
+  const { curComponentId } = useComponetsStore()
+  const [key, setKey] = useState<string>('属性')
+
+  if (!curComponentId) return null
   return (
     <div>
-      <pre>{JSON.stringify(components, null, 2)}</pre>
+      <Segmented
+        value={key}
+        onChange={setKey}
+        block
+        options={['属性', '样式', '事件']}
+      />
+      <div className="pt-[20px]">
+        {key === '属性' && <ComponentAttr />}
+        {key === '样式' && <ComponentStyle />}
+        {key === '事件' && <ComponentEvent />}
+      </div>
     </div>
   )
 }
--- src/stores/component-config.tsx
+++ src/stores/component-config.tsx
@@ -3,10 +3,19 @@ import Container from '@/components/Material/Container'
 import Page from '@/components/Material/Page'
 import { create } from 'zustand'
 
+export interface ComponentSetter {
+  name: string
+  label: string
+  type: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any
+}
+
 export interface ComponentConfig {
   name: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   defaultProps: Record<string, any>
+  setter?: ComponentSetter[]
   desc: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   component: any
@@ -34,6 +43,22 @@ export const useComponentConfigStore = create<State & Action>((set) => ({
         type: 'primary',
         text: '按钮',
       },
+      setter: [
+        {
+          name: 'type',
+          label: '按钮类型',
+          type: 'select',
+          options: [
+            { label: '主按钮', value: 'primary' },
+            { label: '次按钮', value: 'default' },
+          ],
+        },
+        {
+          name: 'text',
+          label: '文本',
+          type: 'input',
+        },
+      ],
       desc: '按钮',
       component: Button,
     },
